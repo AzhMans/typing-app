9ac9b9d245e5d847814bb591851a4014e8eb4012
B14-20 <b14-20@cu.local>
2021-12-01 21:36:19 +0400

    two new files

---
+++ test.js
@@ -0,0 +1,10 @@
+let time  = 10 
+let score  = 0 
+let isPlaying;
+
+const wordInput = document.querySelector ('#word-input');
+const currentWord = document.querySelector ('#current-word');
+const scoreDisplay = document.querySelector ('#score');
+const timeDisplay = document.querySelector ('#time');
+const message = document.querySelector ('#message');
+const seconds = document.querySelector ('#time');
